61b836231525cf86c6d37510cea7db2d78a1df15
Sushi Squad <team7461@gmail.com>
2025-01-09 19:49:53 -0800

    working on swerve

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,20 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import SushiFrcLib.Swerve.SwerveConstants.SwerveModuleConstants;
+import SushiFrcLib.Control.PIDConfig;
+import SushiFrcLib.Motor.MotorConfig;
+import SushiFrcLib.Swerve.SwerveConstants.SDSModules;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.robot.*;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -22,5 +36,65 @@ public final class Constants {
     public static final int PIGEON_ID = 13;
   }
 
-  
+  public static final class Swerve {
+    public static final boolean GYRO_INVERSION = false;
+
+    /* Drivetrain Constants */
+    public static final double TRACK_WIDTH = Units.inchesToMeters(23);
+    public static final double WHEEL_BASE = Units.inchesToMeters(23);
+    public static final double DRIVE_BASE_RADIUS = Math.sqrt(TRACK_WIDTH * TRACK_WIDTH + WHEEL_BASE * WHEEL_BASE) / 2;
+
+    public static final SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
+        new Translation2d(WHEEL_BASE / 2.0, TRACK_WIDTH / 2.0),
+        new Translation2d(WHEEL_BASE / 2.0, -TRACK_WIDTH / 2.0),
+        new Translation2d(-WHEEL_BASE / 2.0, -TRACK_WIDTH / 2.0),
+        new Translation2d(-WHEEL_BASE / 2.0, TRACK_WIDTH / 2.0)
+    );
+
+    // Updated PID values for NEO motors - these will need tuning
+    public static final MotorConfig ANGLE_CONFIG = new MotorConfig(
+        20, // Current limit
+        false,
+        PIDConfig.getPid(0.3), // P value for position control
+        MotorConfig.Mode.COAST
+    );
+
+    public static final MotorConfig ANGLE_FLIPPED_CONFIG = new MotorConfig(
+        20,
+        true,
+        PIDConfig.getPid(0.3),
+        MotorConfig.Mode.COAST
+    );
+
+    public static final MotorConfig DRIVE_CONFIG = new MotorConfig(
+        40,
+        true,
+        PIDConfig.getPid(0.1, 0.5), // P and F values for velocity control
+        MotorConfig.Mode.BRAKE
+    );
+
+    public static final MotorConfig DRIVE_FLIPPED_CONFIG = new MotorConfig(
+        40,
+        false,
+        PIDConfig.getPid(0.1, 0.5),
+        MotorConfig.Mode.BRAKE
+    );
+
+    public static final PIDConfig autoRotate = PIDConfig.getPid(0.008, 0.00, 0.00);
+    public static final SDSModules MODULE_TYPE = SDSModules.MK4i;
+    public static final boolean SWERVE_TUNING_MODE = false;
+
+    public static final SwerveModuleConstants[] SWERVE_MODULE_CONSTANTS = new SwerveModuleConstants[] {
+      new SwerveModuleConstants(0, Rotation2d.fromDegrees(0.461182 * 360), MODULE_TYPE,
+          SWERVE_TUNING_MODE, DRIVE_CONFIG, ANGLE_CONFIG),
+      new SwerveModuleConstants(1, Rotation2d.fromDegrees(0.561035 * 360), MODULE_TYPE,
+          SWERVE_TUNING_MODE, DRIVE_FLIPPED_CONFIG, ANGLE_FLIPPED_CONFIG),
+      new SwerveModuleConstants(2, Rotation2d.fromDegrees(0.570557 * 360), MODULE_TYPE,
+          SWERVE_TUNING_MODE, DRIVE_CONFIG, ANGLE_CONFIG),
+      new SwerveModuleConstants(3, Rotation2d.fromDegrees(0.336426 * 360 + 180), MODULE_TYPE,
+          SWERVE_TUNING_MODE, DRIVE_FLIPPED_CONFIG, ANGLE_FLIPPED_CONFIG)
+    };
+  }
+
+
 }
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -4,17 +4,126 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.List;
 
-public class Swerve extends SubsystemBase {
-  /** Creates a new Swerve. */
-  public Swerve() {
-    
-  }
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+import SushiFrcLib.Sensors.gyro.Pigeon;
+import SushiFrcLib.SmartDashboard.TunableNumber;
+import SushiFrcLib.Swerve.SwerveModules.*;
+import SushiFrcLib.Swerve.SwerveTemplates.VisionBaseSwerve;
 
-  }
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants;
+
+public class Swerve extends VisionBaseSwerve {
+    private static Swerve instance;
+
+    private boolean locationLock;
+    private PIDController rotationLockPID;
+
+    // private final PhotonCamera camera;
+    // private final PhotonPoseEstimator camFilter;
+    // private final TunableNumber maxDistanceCamToTarget;
+
+    public static Swerve getInstance() {
+        if (instance == null) {
+            instance = new Swerve();
+        }
+
+        return instance;
+    }
+
+    private Swerve() {
+        super(
+          new SwerveModuleNeo[] {
+            new SwerveModuleNeo(Constants.Swerve.SWERVE_MODULE_CONSTANTS[0]),
+            new SwerveModuleNeo(Constants.Swerve.SWERVE_MODULE_CONSTANTS[1]),
+            new SwerveModuleNeo(Constants.Swerve.SWERVE_MODULE_CONSTANTS[2]),
+            new SwerveModuleNeo(Constants.Swerve.SWERVE_MODULE_CONSTANTS[3]),
+          },
+          new Pigeon(Constants.Ports.PIGEON_ID, Constants.Swerve.GYRO_INVERSION, Constants.Ports.CANIVORE_NAME),
+          Constants.Swerve.SWERVE_KINEMATICS);
+
+        locationLock = false;
+        rotationLockPID = Constants.Swerve.autoRotate.getPIDController();
+
+        // camera = new PhotonCamera("23");
+        // camFilter = new PhotonPoseEstimator(
+        //         AprilTagFields.k2024Crescendo.loadAprilTagLayoutField(),
+        //         PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+        //         camera,
+        //         new Transform3d());
+        // maxDistanceCamToTarget = new TunableNumber("Max Cam->Target (m)", 6, Constants.TUNING_MODE);
+    }
+
+    public void enableRotationLock(double angle) {
+        locationLock = true;
+
+        rotationLockPID.setSetpoint(angle);
+        rotationLockPID.calculate(getGyro().getAngle().getDegrees());
+    }
+
+    public Command rotateCommand(double angle) {
+        return enableRotationLockCommand(angle).until(() -> {
+            return Math.abs(getGyro().getAngle().getDegrees() - angle) < 2;
+        }).andThen(disableRotationLockCommand());
+    }
+
+    public Command enableRotationLockCommand(double angle) {
+        return runOnce(() -> enableRotationLock(angle));
+    }
+
+    public Command disableRotationLockCommand() {
+        return runOnce(() -> disableRotationLock());
+    }
+
+    public void disableRotationLock() {
+        locationLock = false;
+    }
+
+    @Override
+    public void drive(Translation2d translation, double rotation, Alliance color) {
+        if (locationLock) {
+            rotation = rotationLockPID.calculate(getGyro().getAngle().getDegrees());
+        }
+
+        super.drive(translation, rotation, color);
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        // if (!camera.isConnected())
+        //     return;
+
+        // var pose = camFilter.update();
+
+        // if (!pose.isPresent() || pose.get().targetsUsed.size() < 2)
+        //     return;
+
+        // var targetsCloseEnough = true;
+        // for (var target : pose.get().targetsUsed) {
+        //     var transform = target.getBestCameraToTarget();
+        //     double cameraToTagDistance = new Pose3d().transformBy(transform).getTranslation().getNorm();
+        //     if (cameraToTagDistance > maxDistanceCamToTarget.get()) {
+        //         targetsCloseEnough = false;
+        //         break;
+        //     }
+        // }
+        // if (targetsCloseEnough) {
+        //     super.addVisionTargets(List.of(pose.get()));
+        // }
+
+    }
 }
