2f3e36fe7d38b809e58e1fbe6148c2f92689c361
Sushi Squad <team7461@gmail.com>
2025-01-14 18:50:38 -0800

    fix alliance check

--- src/main/java/frc/robot/AllianceService.java
+++ src/main/java/frc/robot/AllianceService.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 public class AllianceService {
     private static Alliance alliance;
     public static Alliance getAlliance() {
-        if(alliance != null) {
+        if(alliance == null) {
             alliance = DriverStation.getAlliance().get();
         }
         return alliance;
